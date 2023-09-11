84385a651e9dde53e97824e155e891a4c7d588db
LinkedList <martin.macko@productboard.com>
2023-09-11 12:15:53 +0200

    Initial commit with test method

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+/**
+ * Entry point for the assignment. You can test your code in the main function.
+ */
+fun main() {
+    test()
+}
+
+fun test() {
+    TODO("To be implemented according to the assignment")
+}
